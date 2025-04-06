6b437613f16073c2351c7bd9b4c80120d30ddf0f
mobinapn <m.paknia81@gmail.com>
2025-04-06 14:02:32 +0330

    Initialize the project

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,6 +1,19 @@
 <template>
+<<<<<<< HEAD
   <q-item clickable tag="a" target="_blank" :href="props.link">
     <q-item-section v-if="props.icon" avatar>
+=======
+  <q-item
+    clickable
+    tag="a"
+    target="_blank"
+    :href="props.link"
+  >
+    <q-item-section
+      v-if="props.icon"
+      avatar
+    >
+>>>>>>> 346e535 (Initialize the project 🚀)
       <q-icon :name="props.icon" />
     </q-item-section>
 
@@ -15,22 +28,39 @@
 const props = defineProps({
   title: {
     type: String,
+<<<<<<< HEAD
     required: true,
+=======
+    required: true
+>>>>>>> 346e535 (Initialize the project 🚀)
   },
 
   caption: {
     type: String,
+<<<<<<< HEAD
     default: '',
+=======
+    default: ''
+>>>>>>> 346e535 (Initialize the project 🚀)
   },
 
   link: {
     type: String,
+<<<<<<< HEAD
     default: '#',
+=======
+    default: '#'
+>>>>>>> 346e535 (Initialize the project 🚀)
   },
 
   icon: {
     type: String,
+<<<<<<< HEAD
     default: '',
   },
+=======
+    default: ''
+  }
+>>>>>>> 346e535 (Initialize the project 🚀)
 })
 </script>
